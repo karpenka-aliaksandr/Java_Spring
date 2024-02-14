8564822737652a2b8463a157538ba1390522a306
Aliaksandr Karpenka <kasasha@mail.ru>
2024-02-14 16:31:12 +0300

    hw09_tasks

---
+++ HomeWork09/Tasks/src/test/java/gb/karpenka/tasks/TasksApplicationTests.java
@@ -0,0 +1,13 @@
+package gb.karpenka.tasks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TasksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
