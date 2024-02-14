b8ad753de78bfbf4b78d404badef90e68a5bfdef
Aliaksandr Karpenka <kasasha@mail.ru>
2024-02-14 16:33:42 +0300

    hw09_ApiGateWay

---
+++ HomeWork09/ApiGateWay_Eureka/src/main/java/com/example/ApiGateWay_Eureka/ApiGateWayEurekaApplication.java
@@ -0,0 +1,23 @@
+package com.example.ApiGateWay_Eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class ApiGateWayEurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGateWayEurekaApplication.class, args);
+    }
+
+    @Bean
+    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route("Microservice1",r->r.path("/serviceA/**")
+                        .uri("http://localhost:8081/"))
+                .route("Microservice2",r->r.path("/serviceB/**")
+                        .uri("http://localhost:8082/")).build();}
+}
