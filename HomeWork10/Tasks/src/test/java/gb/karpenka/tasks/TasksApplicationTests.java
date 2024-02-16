4b621261233dcf2115a3f6f1f4059b8e9df7e571
Aliaksandr Karpenka <kasasha@mail.ru>
2024-02-16 14:33:33 +0300

    hw10_test

---
+++ HomeWork10/Tasks/src/test/java/gb/karpenka/tasks/TasksApplicationTests.java
@@ -0,0 +1,16 @@
+package gb.karpenka.tasks;
+
+import gb.karpenka.tasks.repositories.TaskRepository;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TasksApplicationTests {
+
+	@Test
+	void contextLoads() {
+
+	}
+
+}
